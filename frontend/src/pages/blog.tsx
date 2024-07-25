8a9f0f2fbcac6fcf3317d1606d82b201fe3e9a32
Sandrine Poissonnet <spoisson@gmail.com>
2024-07-25 06:21:19 -0700

    frontend initial set-up

---
+++ frontend/src/pages/blog.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function BlogPage() {
+	return (
+		<>
+			<Head>
+				<title>Flow Blog - Articles</title>
+				<meta name="description" content="Read the latest posts on Flow Blog" />
+			</Head>
+			<div className="">Hello, Blog!</div>
+		</>
+	);
+}
